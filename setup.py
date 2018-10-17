fb2f57bf3cb3f3d83bd03fbf0d897d7c6ffaca21
Max Pumperla <max.pumperla@googlemail.com>
2018-10-17 17:48:48 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages
 
 setup(
     name='skil',
-    version='0.1',
+    version='0.2',
     packages=find_packages(),
     install_requires=['skil_client', 'requests'],
     extras_require={
