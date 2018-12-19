b85699dfc126428fe600cc4fff23979555e15fd0
Max Pumperla <max.pumperla@googlemail.com>
2018-12-19 23:02:18 +0100

    clean

--- examples/tensorflow-yolo/quickest_yolo.py
+++ examples/tensorflow-yolo/quickest_yolo.py
@@ -4,7 +4,7 @@ import cv2
 model = skil.Model('yolo_v2.pb', model_id='yolo_1', name='yolo')
 service = model.deploy(skil.Deployment(), input_names=['input'], output_names=['output'])
 
-image = cv2.imread("say_yolo_again.jpg", mode='RGB')
+image = cv2.imread("say_yolo_again.jpg")
 detection = service.detect_objects(image)
 image = skil.utils.yolo.annotate_image(image, detection)
-cv2.imshow('yolo', image)
+cv2.imwrite('annotated.jpg', image)
