52b4083142ca6df6b5b02f1881ae03b1e57f974a
Fariz Rahman <farizrahman4u@gmail.com>
2019-01-02 23:44:18 +0530

    fix examples

--- examples/tensorflow-mnist-mlp/run_skil_minimal.py
+++ examples/tensorflow-mnist-mlp/run_skil_minimal.py
@@ -1,15 +1,13 @@
 from skil import Skil, WorkSpace, Experiment, Model, Deployment
 from keras.datasets import mnist
-import os
 
-model_path = os.path.join('model', 'model.pb')
 
 skil_server = Skil()
 work_space = WorkSpace(skil_server)
 experiment = Experiment(work_space)
-model = Model(model_path, name='tf_model', experiment=experiment)
+model = Model('model.pb', model_id='tf_model', experiment=experiment)
 
-deployment = Deployment(skil_server, "tf_model")
+deployment = Deployment(skil_server)
 service = model.deploy(deployment)
 
 
@@ -17,6 +15,7 @@ service = model.deploy(deployment)
 x_test = x_test.reshape(10000, 784)
 x_test = x_test.astype('float32')
 x_test /= 255
+
 
 print(service.predict_single(x_test[0]))
 print(service.predict(x_test[:10]))
