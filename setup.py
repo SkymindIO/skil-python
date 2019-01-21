e5167e2ec0a311e7bea29d1844e1a72c10e33934
Max Pumperla <max.pumperla@googlemail.com>
2019-01-21 21:16:54 +0100

    release version 1.2.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages
 
 setup(
     name='skil',
-    version='0.2.8',
+    version='1.2.0',
     packages=find_packages(),
     install_requires=[
         'skil_client>=1.2.1', 
